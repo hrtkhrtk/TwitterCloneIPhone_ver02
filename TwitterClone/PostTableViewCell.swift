891a70ab5d055a29b2598f2b34bf125148450490
hrtkhrtk <nddgs@yahoo.co.jp>
2020-01-09 16:50:47 +0900

    basic setting 2

---
+++ TwitterClone/PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  TwitterClone
+//
+//  Created by hirotaka.iwasaki on 2020/01/09.
+//  Copyright © 2020 hrtkhrtk. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ TwitterClone/UserTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserTableViewCell.swift
+//  TwitterClone
+//
+//  Created by hirotaka.iwasaki on 2020/01/09.
+//  Copyright © 2020 hrtkhrtk. All rights reserved.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
