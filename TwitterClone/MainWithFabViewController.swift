6ede425f0e6c3f1255fdfde9dc502baf989f80b0
hrtkhrtk <nddgs@yahoo.co.jp>
2020-01-10 03:49:50 +0900

    add FloatingActionButton

---
+++ TwitterClone/MainWithFabViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainWithFabViewController.swift
+//  TwitterClone
+//
+//  Created by hirotaka.iwasaki on 2020/01/10.
+//  Copyright © 2020 hrtkhrtk. All rights reserved.
+//
+
+import UIKit
+
+class MainWithFabViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
