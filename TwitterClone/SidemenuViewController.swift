48ca4b3c19a99906212dba30ce14c2f2c60087ca
hrtkhrtk <nddgs@yahoo.co.jp>
2020-01-09 22:19:14 +0900

    add Sidemenu Ref: https://qiita.com/a_jike/items/7ef99bbc9e69c73353f7

---
+++ TwitterClone/SidemenuViewController.swift
@@ -0,0 +1,135 @@
+//
+//  SidemenuViewController.swift
+//  TwitterClone
+//
+//  Created by hirotaka.iwasaki on 2020/01/09.
+//  Copyright © 2020 hrtkhrtk. All rights reserved.
+//
+
+import UIKit
+
+protocol SidemenuViewControllerDelegate: class {
+    func sidemenuViewControllerDidRequestShowing(_ sidemenuViewController: SidemenuViewController, contentAvailability: Bool, animated: Bool)
+    func sidemenuViewControllerDidRequestHiding(_ sidemenuViewController: SidemenuViewController, animated: Bool)
+    func sidemenuViewController(_ sidemenuViewController: SidemenuViewController, didSelectItemAt indexPath: IndexPath)
+}
+
+class SidemenuViewController: UIViewController {
+    private let contentView = UIView(frame: .zero)
+    private let tableView = UITableView(frame: .zero, style: .plain)
+    private var screenEdgePanGestureRecognizer: UIScreenEdgePanGestureRecognizer!
+    private var panGestureRecognizer: UIPanGestureRecognizer!
+    weak var delegate: SidemenuViewControllerDelegate?
+    private var beganLocation: CGPoint = .zero
+    private var beganState: Bool = false
+    var isShown: Bool {
+        return self.parent != nil
+    }
+    private var contentMaxWidth: CGFloat {
+        return view.bounds.width * 0.8
+    }
+    private var contentRatio: CGFloat {
+        get {
+            return contentView.frame.maxX / contentMaxWidth
+        }
+        set {
+            let ratio = min(max(newValue, 0), 1)
+            contentView.frame.origin.x = contentMaxWidth * ratio - contentView.frame.width
+            contentView.layer.shadowColor = UIColor.black.cgColor
+            contentView.layer.shadowRadius = 3.0
+            contentView.layer.shadowOpacity = 0.8
+            
+            view.backgroundColor = UIColor(white: 0, alpha: 0.3 * ratio)
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        var contentRect = view.bounds
+        contentRect.size.width = contentMaxWidth
+        contentRect.origin.x = -contentRect.width
+        contentView.frame = contentRect
+        contentView.backgroundColor = .white
+        contentView.autoresizingMask = .flexibleHeight
+        view.addSubview(contentView)
+        
+        tableView.frame = contentView.bounds
+        tableView.separatorInset = .zero
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Default")
+        contentView.addSubview(tableView)
+        tableView.reloadData()
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(backgroundTapped(sender:)))
+        tapGestureRecognizer.delegate = self
+        view.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc private func backgroundTapped(sender: UITapGestureRecognizer) {
+        hideContentView(animated: true) { (_) in
+            //self.willMove(toParentViewController: nil)
+            self.willMove(toParent: nil)
+            //self.removeFromParentViewController()
+            self.removeFromParent()
+            self.view.removeFromSuperview()
+        }
+    }
+
+    func showContentView(animated: Bool) {
+        if animated {
+            UIView.animate(withDuration: 0.3) {
+                self.contentRatio = 1.0
+            }
+        } else {
+            contentRatio = 1.0
+        }
+    }
+    
+    func hideContentView(animated: Bool, completion: ((Bool) -> Swift.Void)?) {
+        if animated {
+            UIView.animate(withDuration: 0.2, animations: {
+                self.contentRatio = 0
+            }, completion: { (finished) in
+                completion?(finished)
+            })
+        } else {
+            contentRatio = 0
+            completion?(true)
+        }
+    }
+}
+
+extension SidemenuViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Default", for: indexPath)
+        cell.textLabel?.text = "Item \(indexPath.row)"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        delegate?.sidemenuViewController(self, didSelectItemAt: indexPath)
+        
+        print("testtest")
+        print(indexPath.row)
+    }
+}
+
+extension SidemenuViewController: UIGestureRecognizerDelegate {
+    internal func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        let location = gestureRecognizer.location(in: tableView)
+        if tableView.indexPathForRow(at: location) != nil {
+            return false
+        }
+        return true
+    }
+}
--- TwitterClone/ViewController.swift
+++ TwitterClone/ViewController.swift
@@ -9,12 +9,88 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    //let mainViewController = self.storyboard?.instantiateViewController(withIdentifier: "Main")
+    //let contentViewController = UINavigationController(rootViewController: UIViewController())
+    let sidemenuViewController = SidemenuViewController()
+    private var isShownSidemenu: Bool {
+        return sidemenuViewController.parent == self
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //menuButton.addTarget(self, action:#selector(sidemenuBarButtonTapped(sender:)), for: .touchUpInside)
+        menuButton.target = self // 参考：https://stackoverflow.com/questions/2333638/how-to-set-target-and-action-for-uibarbuttonitem-at-runtime 「Just set the UIBarButtonItem's target and action properties directly.」
+        menuButton.action = #selector(sidemenuBarButtonTapped(sender:))
+        
+        let mainViewController = self.storyboard?.instantiateViewController(withIdentifier: "Main")
+        //addChildViewController(mainViewController!)
+        addChild(mainViewController!)
+        view.addSubview(mainViewController!.view)
+        //mainViewController!.didMove(toParentViewController: self)
+        mainViewController!.didMove(toParent: self)
+    }
+    
+    @objc private func sidemenuBarButtonTapped(sender: Any) {
+        showSidemenu(animated: true)
     }
 
+    private func showSidemenu(contentAvailability: Bool = true, animated: Bool) {
+        if isShownSidemenu { return }
+        
+        let mainViewController = self.storyboard?.instantiateViewController(withIdentifier: "Main")
+        
+        //addChildViewController(sidemenuViewController)
+        addChild(sidemenuViewController)
+        sidemenuViewController.view.autoresizingMask = .flexibleHeight
+        //sidemenuViewController.view.frame = contentViewController.view.bounds
+        sidemenuViewController.view.frame = mainViewController!.view.bounds
+        //view.insertSubview(sidemenuViewController.view, aboveSubview: contentViewController.view)
+        view.insertSubview(sidemenuViewController.view, aboveSubview: mainViewController!.view)
+        //sidemenuViewController.didMove(toParentViewController: self)
+        sidemenuViewController.didMove(toParent: self)
+        if contentAvailability {
+            sidemenuViewController.showContentView(animated: animated)
+        }
+    }
 
+    private func hideSidemenu(animated: Bool) {
+        if !isShownSidemenu { return }
+        
+        sidemenuViewController.hideContentView(animated: animated, completion: { (_) in
+            //self.sidemenuViewController.willMove(toParentViewController: nil)
+            self.sidemenuViewController.willMove(toParent: nil)
+            //self.sidemenuViewController.removeFromParentViewController()
+            self.sidemenuViewController.removeFromParent()
+            self.sidemenuViewController.view.removeFromSuperview()
+        })
+    }
 }
 
+//extension MainViewController: SidemenuViewControllerDelegate {
+extension ViewController: SidemenuViewControllerDelegate {
+    func parentViewControllerForSidemenuViewController(_ sidemenuViewController: SidemenuViewController) -> UIViewController {
+        return self
+    }
+    
+    func shouldPresentForSidemenuViewController(_ sidemenuViewController: SidemenuViewController) -> Bool {
+        /* You can specify sidemenu availability */
+        return true
+    }
+    
+    func sidemenuViewControllerDidRequestShowing(_ sidemenuViewController: SidemenuViewController, contentAvailability: Bool, animated: Bool) {
+        //showSidemenu(contentAvailability: contentAvailability, animated: animated)
+        showSidemenu(contentAvailability: contentAvailability, animated: animated)
+    }
+    
+    func sidemenuViewControllerDidRequestHiding(_ sidemenuViewController: SidemenuViewController, animated: Bool) {
+        hideSidemenu(animated: animated)
+    }
+    
+    func sidemenuViewController(_ sidemenuViewController: SidemenuViewController, didSelectItemAt indexPath: IndexPath) {
+        hideSidemenu(animated: true)
+    }
+}
